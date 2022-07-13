7f4bd48fcdde22d24143b5a17fd64b1d5c9a1918
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-07-13 18:29:19 +0530

    delete and mark tasks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,29 @@ import React, {useState}from 'react'
 import Tasks from './Tasks'
 import BgImg from "../images/bg-desktop-dark.jpg" 
 import ToggleImg from "../images/icon-sun.svg"
-export default function SearchBar(){   
+export default function SearchBar(props){   
     const [list,setList]=useState([])
     let sendData = (e)=>{        
         if(e.key=="Enter"){           
             setList([...list,e.target.value]) 
             e.target.value=""                        
         }
+    }
+    let [mark,setMark]=useState([])
+
+    let markEl=(id)=>{
+      list.filter((el,i=0)=>{
+          if(i==id){setMark([...mark,id])}
+      })
+      console.log(mark)
+    }
+    let removeEntry = (id)=>{
+      setList (list.filter((el,i=0)=>{
+        if(i!=id){
+          return true
+        }
+        i++
+      }))
     }
     return <>
     <img className="bg-img" src={BgImg} alt='coming soon' />
@@ -29,7 +45,7 @@ export default function SearchBar(){
         list.map((item,i=0)=>{
             
            return <>
-           <Tasks task={item} id={i++}></Tasks>
+           <Tasks task={item} id={i++} removeEntry={removeEntry} markEl={markEl}></Tasks>
            <hr className='line'></hr>
            </>
         })}
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cross from "../images/icon-cross.svg"
+import Check from "../images/icon-check.svg"
 
 export default function Tasks(props){
+    
     return <li key={props.id}>
     <div className='txt-box flex'>
-        <div className='circle'></div>
-        <span className='txt'>{props.task}</span>
-        <img src={Cross} className='flex-end cross' alt="cross"></img>
+        <div className='circle' onClick={(id)=>{props.markEl(props.id)}}><img onClick={(e)=>{ console.log(e)
+            e.target.style.display="block"}} className="check" src={Check}/></div>
+        <p className='txt'>{props.task}</p>
+        {/* {console.log(props)} */}
+        <img src={Cross} className='cross' alt="cross" onClick={()=>{props.removeEntry(props.id)}}></img>
     </div>
     </li>
 }
